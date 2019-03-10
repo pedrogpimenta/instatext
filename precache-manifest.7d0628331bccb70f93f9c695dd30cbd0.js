76d9b361a27bfb14ef0c9f7c32497bc01e683124
Pedro Pimenta <pedro@pimenta.co>
2019-03-10 09:49:32 +0100

    Updates

---
+++ precache-manifest.7d0628331bccb70f93f9c695dd30cbd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc16f460e51ddf56abf8",
+    "url": "/instatext/static/js/runtime~main.fc16f460.js"
+  },
+  {
+    "revision": "9ef43bb9494d9c8bbe40",
+    "url": "/instatext/static/js/main.9ef43bb9.chunk.js"
+  },
+  {
+    "revision": "0b555523f6c092282902",
+    "url": "/instatext/static/js/2.0b555523.chunk.js"
+  },
+  {
+    "revision": "9ef43bb9494d9c8bbe40",
+    "url": "/instatext/static/css/main.affe257a.chunk.css"
+  },
+  {
+    "revision": "d584be30548c566b52930d072978e183",
+    "url": "/instatext/index.html"
+  }
+];
