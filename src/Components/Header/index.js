b2cec47f8b2077595d4fcc98871fabe42477f65e
Pedro Pimenta <pedro@pimenta.co>
2019-03-10 12:40:29 +0100

    Fix title

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -30,7 +30,7 @@ class Header extends Component {
               color='inherit'
               className={styles.grow}
             >
-              InstaTexta
+              InstaText
             </Typography>
           </Toolbar>
         </AppBar>
