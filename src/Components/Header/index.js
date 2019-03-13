d3538fc1ed930a5a8b8c4f460d11489af4945c08
Pedro Pimenta <pedro@pimenta.co>
2019-03-13 11:32:09 +0100

    Fix errors

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -3,7 +3,6 @@ import styles from './index.module.css';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
